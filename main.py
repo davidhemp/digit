f8c9efda4ed301a7cee9c2155b787e5af6402388
veioenza <cyber_veio@hotmail.com>
2023-04-09 21:41:49 +0100

    Load 10k data and ascii print an image to test

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
 #!/bin/env python3
-from tensorflow.examples.tutorials.mnist import input_data
+import gzip
+import numpy as np
 
+def load_images(filename, num_images):
+    with gzip.open(filename, 'r') as f:
+        image_size = 28
+        f.read(16)
+        buffer = f.read(image_size * image_size * num_images)
+        data = np.frombuffer(buffer,  dtype=np.uint8).astype(np.float32)
+        data =  data.reshape(num_images, image_size, image_size, 1)
+    return data
 
-mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)  # y labels are oh-encoded
+def load_labels(filename, num_labels):
+    with gzip.open("MNIST_data/t10k-labels-idx1-ubyte.gz", 'r') as f:
+        f.read(8)
+        buffer = f.read(num_labels)
+        labels = np.frombuffer(buffer,  dtype=np.uint8).astype(np.float32)
+    return labels
+
+def ascii_image(image, label):
+    print(f"Image labeled as {label}")
+    for line in image:
+        print("".join([ "{:03d}".format(int(i)) for i in line]))
+
+test_images = load_images("MNIST_data/t10k-images-idx3-ubyte.gz", 10000)
+test_labels = load_labels("MNIST_data/t10k-labels-idx1-ubyte.gz", 10000)
+
+idx = 100
+ascii_image(test_images[idx], test_labels[idx])
+
