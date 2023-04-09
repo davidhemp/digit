a61621dd549426075eb5f96b1d226f3d78e029ed
veioenza <cyber_veio@hotmail.com>
2023-04-09 18:38:11 +0100

    Switched to using TensorFlow/2.7.1-foss-2021b-CUDA-11.4.1

--- test-gpu.py
+++ test-gpu.py
@@ -1,4 +1,5 @@
 #!/bin/env python3
 import tensorflow as tf
 
-print('Num GPUs Available: ', len(tf.config.list_physical_devices('GPU')))
+gpus=tf.config.list_physical_devices("GPU")
+print(f"GPUs Available: {gpus}")
