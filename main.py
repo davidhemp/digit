dd562a4f3871dfe4fcb981c21dc620f56ddbd32c
veioenza <cyber_veio@hotmail.com>
2023-04-10 02:53:08 +0100

    Working Single hidden layer, about 0.8 accuracy after 500 iteractions

--- main.py
+++ main.py
@@ -10,16 +10,117 @@ test_labels = load_labels("MNIST_data/t10k-labels-idx1-ubyte.gz", 10000)
 train_images = load_images("MNIST_data/train-images-idx3-ubyte.gz", 60000)
 train_labels = load_labels("MNIST_data/train-labels-idx1-ubyte.gz", 60000)
 
-train_images, validation_images = train_images[:55000], train_images[55000:]
-train_labels, validation_labels = train_labels[:55000], train_labels[55000:]
-
 #print one image from each set to show it is working
-debug = 0
-if debug:
-    idx = 123
-    ascii_image(test_images[idx], test_labels[idx])
-    ascii_image(train_images[idx], train_labels[idx])
-    ascii_image(validation_images[idx], validation_labels[idx])
+idx = 666
+ascii_image(train_images[idx], train_labels[idx])
+
+#Transpose the image so each column is now an image. 
+#Or to say another way, each row is now a single pixel with a value for each image
+#Data is also normalised to stop exp overflow
+train_data = train_images.T/255
+
+#Convert a label to an array where the index matching the label is 1, all else are 0
+#I have used enumerate to remind me what this is doing, assignment would be faster
+train_truth = np.zeros((train_labels.size, 10))
+for i, label in enumerate(train_labels):
+    train_truth[i, int(label)] = 1
+train_truth = train_truth.T
+print(f"One Hot y: {train_truth.T[idx]}")
+
+#Define NN layers
+
+n_input = train_data.shape[0]
+n_samples = train_data.shape[1]
+n_hidden = 10
+n_output = 10
+
+#Activation functions
+
+def ReLU(x: np.ndarray) -> np.ndarray:
+    """Applies a very simple ReLU activation function to a numpy array"""
+    return np.maximum(0, x)
+
+def ReLU_derivative(Z):
+    return Z > 0
+
+def softmax(x):
+    """Compute softmax values for each sets of scores in x."""
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum(axis=0)
+
+# Generate init values for layers
+
+weights_1 = np.random.rand(n_hidden, n_input) - 0.5
+bias_1 = np.random.rand(n_hidden, n_samples) - 0.5
+
+weights_2 = np.random.randn(n_output, n_output) - 0.5
+bias_2 = np.random.randn(n_output, n_samples) - 0.5
+#Putting values into a list just for management
+params = [weights_1, bias_1, weights_2, bias_2]
+# Apply weights and bias then activate
+def forward_prop(input_layer, params):
+    weights_1, bias_1, weights_2, bias_2 = params
+    pre_activation_1 = np.dot(weights_1, input_layer) + bias_1
+    hidden_layer_1 = ReLU(pre_activation_1)
+    pre_activation_2 = np.dot(weights_2, hidden_layer_1) + bias_2
+    output_layer = softmax(pre_activation_2)
+    return [input_layer, pre_activation_1, hidden_layer_1, pre_activation_2, output_layer]
+
+def back_prop(nn, params, n_samples, true_y):
+    weights_1, bias_1, weights_2, bias_2 = params
+    input_layer, pre_activation_1, hidden_layer_1, pre_activation_2, output_layer = nn
+    #Detrivative of softmax simplifies to just the differance
+    pre_activation_2_error = output_layer - true_y
+    delta_weights_2 = np.dot(pre_activation_2_error, hidden_layer_1.T)/n_samples
+    delta_bias_2 = np.sum(pre_activation_2_error, 1)/n_samples
+    
+    pre_activation_error_1 = np.dot(weights_2, pre_activation_2_error) * ReLU_derivative(pre_activation_1)
+    delta_weights_1 = np.dot(pre_activation_error_1, input_layer.T) / n_samples
+    delta_bias_1 = np.sum(pre_activation_error_1, 1)/n_samples
+    return [delta_weights_1, delta_bias_1, delta_weights_2, delta_bias_2]
+
+def update_params(params, deltas, alpha=0.1):
+    delta_weights_1, delta_bias_1, delta_weights_2, delta_bias_2 = deltas
+    weights_1, bias_1, weights_2, bias_2 = params
+
+    weights_1 -= alpha * delta_weights_1
+    weights_2 -= alpha * delta_weights_2
+    bias_1 -= alpha * np.reshape(delta_bias_1, (10,1))
+    bias_2 -= alpha * np.reshape(delta_bias_2, (10,1))
+    
+    return [weights_1, bias_1, weights_2, bias_2]
+
+def get_predictions(output_layer):
+    return np.argmax(output_layer, 0)
+
+def get_accuracy(predictions, truth):
+    return np.sum(predictions == truth) / truth.size
+
+iterations=201
+for i in range(iterations):
+
+    #Generate outputs using current parms
+    nn = forward_prop(train_data, params)
+    #Calculate the error and work backwards
+    deltas = back_prop(nn, params, n_samples, train_truth)
+    params = update_params(params, deltas)
+
+    if i % 100 == 0:
+        test_output_layer = nn[4]
+        predictions = get_predictions(test_output_layer)
+        print(predictions[:10], train_labels[:10])
+        accuracy = get_accuracy(predictions, train_labels)
+        print(f"Iteration: {i} has accuracy {accuracy}")
+
+test_image = train_images[idx]/255.
+print(np.unique(get_predictions(forward_prop(test_image.reshape(784,1), params)[4])))
 
 
+#Testing an image I drew in paint
+from PIL import Image
+img = Image.open('2.png').convert('L')
+my_image = np.abs(np.asarray(img).astype(np.float32) - 255)
+ascii_image(my_image, 2)
 
+my_data = my_image.reshape(784, 1)
+print(np.unique(get_predictions(forward_prop(my_data, params)[4])))
